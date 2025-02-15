175a4b0a72c0f27cd144cb4f5ca5b9231b3607f6
victorxtsai <71499306+victorxtsai@users.noreply.github.com>
2025-02-15 22:39:05 +0000

    Update20250215

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MelChat from "./pages/MelChat";
 
+// path "/" means default, if you want another ending, then like "/profile" or something...
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/mel" element={<MelChat />} />
+        <Route path="/" element={<MelChat />} />
       </Routes>
     </Router>
   );
