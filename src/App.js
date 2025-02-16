59b64e081d9f67ca95bf924639cd7be513c53b24
victorxtsai <71499306+victorxtsai@users.noreply.github.com>
2025-02-16 03:01:58 +0000

    Update20250215a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MelChat from "./pages/MelChat";
-import SignIn from './pages/SignIn';
 
 // path "/" means default, if you want another ending, then like "/profile" or something...
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<MelChat />} />
-        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
   );
